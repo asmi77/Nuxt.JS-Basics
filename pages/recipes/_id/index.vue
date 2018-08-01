eb6c911bd8af62950f5f4c39d3e7eb320d802609
Asma <elkabir.asma@gmail.com>
2018-08-01 23:11:58 +0200

    get started with nuxt

---
+++ pages/recipes/_id/index.vue
@@ -0,0 +1,61 @@
+<template>
+    <section class="single-recipe">
+<h1>{{ recipe.title }}</h1>
+<div>
+    <img class="recipe-image" :src="recipe.thumbnail" :alt="recipe.title">
+</div>
+<p>{{ recipe.previewText }}</p>
+    </section>
+</template>
+
+<script>
+    export default {
+         asyncData(context) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve({
+          recipe: [
+            {
+              id: "1",
+              title: "Melba strawberries",
+              previewText: "Fabulous desert",
+              thumbnail:
+                "https://img.taste.com.au/uHDkf_kl/taste/2016/11/peach-melba-with-whipped-ricotta-cream-92563-1.jpeg"
+            },
+            {
+              id: "2",
+              title: "Sea food",
+              previewText: "Tasty diet",
+              thumbnail:
+                "https://www.iheartnaptime.net/wp-content/uploads/2016/03/Easy-and-Delicious-Slow-Cooker-Seafood-Stew-Recipe-.jpg"
+            },
+            {
+              id: "3",
+              title: "Magic Spoon",
+              previewText: "The icing on the cake",
+              thumbnail:
+                "https://i.pinimg.com/736x/8d/8e/21/8d8e212154e453cf6ba323728318acf8--vintage-silver-antique-silver.jpg"
+            }
+          ].find(el => el.id === context.params.id)
+        });
+      }, 1500);
+    });
+  }
+    }
+</script>
+
+<style scoped>
+.single-recipe {
+  display: flex;
+  flex-flow: column; 
+  justify-content: center;
+  align-items: center;
+  text-align: center; 
+  padding: 30px; 
+}
+
+.recipe-image {
+    width: 50%; 
+}
+</style>
+
---
+++ pages/recipes/index.vue
@@ -0,0 +1,64 @@
+<template>
+    <section class="recipes">
+        <Recipe
+        v-for="recipe in recipes"
+        :key="recipe.id"
+        :thumbnail="recipe.thumbnail"
+        :title="recipe.title"
+        :id="recipe.id"
+        :previewText="recipe.previewText"
+        />
+    </section>
+</template>
+<script>
+import Recipe from "@/components/Recipe";
+
+export default {
+  components: {
+    Recipe
+  },
+  asyncData() {
+    // if using a request
+    // return axios.get()
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve({
+          recipes: [
+            {
+              id: "1",
+              title: "Melba strawberries",
+              previewText: "Fabulous desert",
+              thumbnail:
+                "https://img.taste.com.au/uHDkf_kl/taste/2016/11/peach-melba-with-whipped-ricotta-cream-92563-1.jpeg"
+            },
+            {
+              id: "2",
+              title: "Sea food",
+              previewText: "Tasty diet",
+              thumbnail:
+                "https://www.iheartnaptime.net/wp-content/uploads/2016/03/Easy-and-Delicious-Slow-Cooker-Seafood-Stew-Recipe-.jpg"
+            },
+            {
+              id: "3",
+              title: "Magic Spoon",
+              previewText: "The icing on the cake",
+              thumbnail:
+                "https://i.pinimg.com/736x/8d/8e/21/8d8e212154e453cf6ba323728318acf8--vintage-silver-antique-silver.jpg"
+            }
+          ]
+        });
+      }, 1500);
+    });
+  }
+};
+</script>
+
+<style scoped>
+.recipes {
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: center;
+  align-items: center;
+}
+</style>
+
